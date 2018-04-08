a7ace18d82b73814f1c2d0a25969b1ceba7f34a1
andy <andy.speirs2@gmail.com>
2018-04-08 14:11:05 +0100

    Added files

---
+++ gulpfile.js
@@ -0,0 +1,186 @@
+var gulp         = require('gulp');                     // https://www.npmjs.com/package/gulp
+var babel        = require('gulp-babel');               // https://www.npmjs.com/package/gulp-babel
+var babelify     = require('babelify');                 // https://www.npmjs.com/package/babelify
+var browserify   = require('browserify');               // https://www.npmjs.com/package/browserify
+var browserSync  = require('browser-sync').create();    // https://www.npmjs.com/package/browser-sync
+var buffer       = require("vinyl-buffer");             // https://www.npmjs.com/package/vinyl-buffer
+var cleanCSS     = require('gulp-clean-css');           // https://www.npmjs.com/package/gulp-clean-css
+var del          = require('del');                      // https://www.npmjs.com/package/del
+var fs           = require('fs');                       // https://www.npmjs.com/package/file-system
+var hb           = require('gulp-hb');                  // https://www.npmjs.com/package/gulp-hb
+var hbHelper     = require('handlebars-layouts');       // https://www.npmjs.com/package/handlebars-layouts
+var htmlmin      = require('gulp-htmlmin');             // https://www.npmjs.com/package/gulp-htmlmin
+var nop          = require('gulp-nop');                 // https://www.npmjs.com/package/gulp-nop
+var prefix       = require('gulp-autoprefixer');        // https://www.npmjs.com/package/gulp-autoprefixer
+var rename       = require('gulp-rename');              // https://www.npmjs.com/package/gulp-rename
+var runSequence  = require('run-sequence');             // https://www.npmjs.com/package/run-sequence
+var sass         = require('gulp-sass');                // https://www.npmjs.com/package/gulp-sass
+var source       = require('vinyl-source-stream');      // https://www.npmjs.com/package/vinyl-source-stream
+var uglify       = require('gulp-uglify');              // https://www.npmjs.com/package/gulp-uglify
+
+var env = process.env.NODE_ENV;
+var paths = {
+	src: 'src/',
+	build: 'build/',
+	helpers: './helpers/',
+	css: 'assets/css/',
+	data: 'assets/data/',
+	js: 'assets/js/',
+	img: 'assets/img/',
+	layout: 'templates/layout/',
+	pages: 'templates/pages/',
+	partials: 'templates/partials/'
+};
+
+
+// ==========================================================================================
+// ====  Clean  =============================================================================
+// ==========================================================================================
+
+// Cleans img folder
+gulp.task('clean:img', function() {
+	return del(paths.build + paths.img);
+});
+// Clean js folder
+gulp.task('clean:js', function() {
+	return del(paths.build + paths.js);
+});
+// Clean css folder
+gulp.task('clean:css', function() {
+	return del(paths.build + paths.css);
+});
+// Cleans files from root of build
+gulp.task('clean:root', function() {
+	return del([paths.build + '*.*', '!' + paths.build + '*.html']);
+});
+// Clean assets folder
+gulp.task('clean:assets', function() {
+	return del(paths.build + 'assets');
+});
+// Clean html files from root of build
+gulp.task('clean:pages', function() {
+	return del(paths.build + '*.html');
+});
+// Clean build folder
+gulp.task('clean:build', function() {
+	return del(paths.build);
+});
+
+
+// ==========================================================================================
+// ====  Files  =============================================================================
+// ==========================================================================================
+
+// Move img folder contents
+gulp.task('files:img', ['clean:img'], function() {
+	return gulp.src([paths.src + paths.img + '**/*', '!' + paths.src + paths.img + '**/*.{ai,psd,svg}'])
+		.pipe(gulp.dest(paths.build + paths.img));
+});
+// Move js folder contents
+gulp.task('files:js', ['clean:js'], function() {
+	var argBabel = process.argv.includes('--babel');
+	argBabel ? console.log('Serving with babel preset') : nop();
+
+	return browserify(paths.src + paths.js + 'main.js')
+		.transform([
+			"babelify", { presets: ["es2015"] }
+		])
+		.bundle()
+		.pipe(source('main.js'))
+		.pipe(buffer())
+		.pipe(env === 'prod' || argBabel ? uglify() : nop())
+		.pipe(rename({ extname: '.min.js' }))
+		.pipe(gulp.dest(paths.build + paths.js));
+});
+// Compiles scss files
+gulp.task('files:css', ['clean:css'], function() {
+	return gulp.src(paths.src + paths.css + '*.scss')
+		.pipe(sass().on('error', sass.logError))
+		.pipe(prefix())
+		.pipe(cleanCSS())
+		.pipe(rename({ extname: '.min.css' }))
+		.pipe(gulp.dest(paths.build + paths.css));
+});
+// Moves root files file
+gulp.task('files:root', ['clean:root'], function() {
+	return gulp.src([paths.src + '*.*'])
+		.pipe(gulp.dest(paths.build));
+});
+// Compiles Handlebar files
+gulp.task('files:handlebar', ['clean:pages'], function() {
+	return gulp.src(paths.src + paths.layout + '*.hbs')
+		.pipe(hb()
+			.partials(paths.src + paths.pages + '*.hbs')
+			.partials(paths.src + paths.partials + '*.hbs')
+			.helpers(hbHelper)
+			.helpers(paths.helpers + '**/*.js')
+			.data(paths.src + paths.data + '/**/*.json')
+			.data({ debug: env === 'dev' ? true : false })
+			.data(paths.src + 'manifest.json'))
+		.pipe(rename({ extname: '.html' }))
+		.pipe(htmlmin({ collapseWhitespace: true }))
+		.pipe(gulp.dest(paths.build));
+});
+
+
+// ==========================================================================================
+// ====  Environment  =======================================================================
+// ==========================================================================================
+
+// Sets the `files:handlebar` task is complete before reloading the browser
+gulp.task('set-dev', function() {
+	return env = 'dev';
+});
+gulp.task('set-prod', function() {
+	return env = 'prod';
+});
+
+
+// ==========================================================================================
+// ====  Serve  ============================================================================
+// ==========================================================================================
+
+// Watches css, js and handlebar files (using Browsersync) then compiles them to the build folder
+gulp.task('serve', function() {
+	runSequence(['set-dev'], 'files:img', 'files:js', 'files:css', 'files:root', 'files:handlebar');
+
+	// Serve files from the root of this project
+	browserSync.init({
+		server: {
+			baseDir: paths.build,
+			index: 'index.html'
+		},
+		// Don't show any notifications in the browser.
+		notify: false,
+		// Wait 2 seconds after a reload event before allowing more.
+		reloadDebounce: 2000
+	});
+
+	// add browserSync.reload to the tasks array to make
+	// all browsers reload after tasks are complete.
+	gulp.watch(paths.src + paths.img + '**/*', ['files:img']).on('change', browserSync.reload);
+	gulp.watch(paths.src + paths.js + '**/*', ['files:js']).on('change', browserSync.reload);
+	gulp.watch(paths.src + paths.css + '**/*', ['files:css']).on('change', browserSync.reload);
+	gulp.watch(paths.src + paths.data + '**/*', ['files:handlebar']).on('change', browserSync.reload);
+	gulp.watch(paths.src + 'templates/**/*.hbs', ['files:handlebar']).on('change', browserSync.reload);
+	gulp.watch(paths.src + '*.*', ['files:root']).on('change', browserSync.reload);
+	gulp.watch(paths.helpers + '**/*.js', ['files:handlebar']).on('change', browserSync.reload);
+});
+
+
+// ==========================================================================================
+// ====  Build  =============================================================================
+// ==========================================================================================
+
+// Removes html files and everything from assets folder, then compiles to build folder
+gulp.task('build', function() {
+	runSequence(['set-prod'], 'files:img', 'files:js', 'files:css', 'files:root', 'files:handlebar');
+});
+
+
+// ==========================================================================================
+// ====  Default  ===========================================================================
+// ==========================================================================================
+
+// Runs the serve task by default
+gulp.task('default', ['serve']);
---
+++ src/assets/js/main.js
@@ -0,0 +1,104 @@
+import { xml2js } from "xml-js"; // https://www.npmjs.com/package/xml-js
+
+window.addEventListener('load', function() {
+	const plexArt = document.querySelector('#plex-art');
+	const plexGrandparentTitle = document.querySelector('#plex-grandparent-title');
+	const plexTitle = document.querySelector('#plex-title');
+	const plexRating = document.querySelector('#plex-rating');
+
+	const plexIP = localStorage.getItem('plex-settings-ip') || '192.168.0.1';
+	const plexPort = localStorage.getItem('plex-settings-port') || '32400';
+	const plexToken = localStorage.getItem('plex-settings-token') || '';
+
+	const plexSettingsToggle = document.querySelector('.plex-settings-toggle');
+	const plexSettingsContainer = document.querySelector('.plex-settings-container');
+
+	plexSettingsToggle.addEventListener('click', function() {
+		plexSettingsContainer.classList.toggle('active');
+	});
+
+	const plexSettingsForm = document.getElementById('plex-settings-form');
+	const plexSettingsIP = document.getElementById('plex-settings-ip');
+	const plexSettingsPort = document.getElementById('plex-settings-port');
+	const plexSettingsToken = document.getElementById('plex-settings-token');
+	plexSettingsIP.defaultValue = plexIP;
+	plexSettingsPort.defaultValue = plexPort;
+	plexSettingsToken.defaultValue = plexToken;
+
+	function updateLocalStorage() {
+		localStorage.setItem('plex-settings-ip', plexSettingsIP.value);
+		localStorage.setItem('plex-settings-port', plexSettingsPort.value);
+		localStorage.setItem('plex-settings-token', plexSettingsToken.value);
+	}
+
+	function plexSettingsFormSubmit(e) {
+		e.preventDefault();
+		updateLocalStorage();
+	}
+
+	plexSettingsForm.addEventListener('submit', plexSettingsFormSubmit);
+
+
+	const plexURL = `http://${plexIP}:${plexPort}`;
+	const plexURLToken = `${plexURL}/status/sessions?X-Plex-Token=${plexToken}`;
+
+	function plexDisplay(data) {
+		console.log(data);
+
+		const size = data.elements[0].attributes.size;
+		// console.log(size);
+
+		if (size !== '0') {
+			console.log('playing');
+
+			const elements = data.elements[0].elements[0];
+			// console.log(elements);
+
+			// const name = elements.name;
+			// console.log(name);
+
+			const attributes = elements.attributes;
+			// console.log(attributes);
+
+			const art = attributes.art;
+			// const art = attributes.thumb;
+			// const art = attributes.parentThumb;
+			// console.log(art);
+			plexArt.src = `${plexURL}${art}?X-Plex-Token=${plexToken}`;
+
+			const title = attributes.title;
+			// console.log(title);
+			plexTitle.innerHTML = title;
+
+			const grandparentTitle = attributes.grandparentTitle;
+			// console.log(grandparentTitle);
+			plexGrandparentTitle.innerHTML = grandparentTitle;
+
+			const rating = attributes.rating;
+			// console.log(rating);
+			plexRating.innerHTML = `${rating}/10`;
+		} else {
+			plexGrandparentTitle.innerHTML = 'Nothing Playing';
+		}
+
+	}
+
+	fetch(plexURLToken)
+		.then(
+			function(response) {
+				if (response.status !== 200) {
+					console.log(`Looks like there was a problem. Status Code: ${response.status}`);
+					return;
+				}
+				// Do something with the response
+				response.text()
+					.then(function(data) {
+						// console.log(data);
+						plexDisplay(xml2js(data));
+					});
+			}
+		)
+		.catch(function(err) {
+			console.log('Fetch Error', err);
+		});
+});
